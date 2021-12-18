d65184861e28addfc8264874a34d39144a5cd827
ttarnawski <81701027+ttarnawski@users.noreply.github.com>
2021-12-18 16:59:21 +0100

    Add files via upload

--- include/storage_types.hpp
+++ include/storage_types.hpp
@@ -1,29 +1,45 @@
 //
 // Created by michal on 14/12/2021.
 //
+#include <stack>
+#include <list>
+#include "package.hpp"
 
 #ifndef POLPRODUKTY_STORAGE_TYPES_HPP
 #define POLPRODUKTY_STORAGE_TYPES_HPP
 
 enum PackageQueueType{FIFO, LIFO};
 
-class PackageQueue{
-    PackageQueue(PackageQueueType queue_type) : queue_type_(queue_type) {}
+class IPackageStockpile {
+public:
+    //using std::list<Package>::const_iterator = const_iterator; jakos trzeba ten alias zrobic
 
-    PackageQueueType queue_type_;
+    virtual void push() { stockpile_.push_back(Package&&); }
+    virtual size_t size() const { return stockpile_.size(); }
+    virtual bool empty() const { return stockpile_.size() == 0; }
+    //iteratory/;
+    std::list<Package>::const_iterator begin() const { return stockpile_.cbegin(); }
+    std::list<Package>::const_iterator cbegin() const { return stockpile_.cbegin(); }
+    std::list<Package>::const_iterator end() const { return stockpile_.cend(); }
+    std::list<Package>::const_iterator cend() const { return stockpile_.cend(); }
+
+    virtual ~IPackageStockpile() = default;
+
+    std::list<Package> stockpile_;
 };
 
-class IPackageQueue{
+class IPackageQueue: public IPackageStockpile {
+public:
     virtual Package pop() = 0;
     virtual PackageQueueType get_queue_type() {}
 };
 
-class IPackageStockpile{
-    void push(Package&&);
-    bool empty() {query;}
-    size_type size() {query;}
-    /iteratory/;
-    ~IPackageStockpile();
+class PackageQueue: public IPackageQueue {
+public:
+    PackageQueue(PackageQueueType queue_type) : queue_type_(queue_type) {}
+
+    PackageQueueType queue_type_;
+    std::list<Package> queue_;
 };
 
 #endif //POLPRODUKTY_STORAGE_TYPES_HPP
