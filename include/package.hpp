8ceddd3d8d27c36b30fa3c9253fe4aff0eb74bab
Wojciech Tokarz <wtokarz@student.agh.edu.pl>
2021-12-19 00:38:20 +0100

    to wszystko wygląda bardzo źle ogólnie.

--- include/package.hpp
+++ include/package.hpp
@@ -5,14 +5,19 @@
 
 #ifndef POLPRODUKTY_PACKAGE_HPP
 #define POLPRODUKTY_PACKAGE_HPP
+#include <set>
 
 class Package {
 public:
     Package() = default;
     Package(ElementID id) : id_(id) {}
     Package(Package&&) = default;
+
+    static std::set<ElementID> assigned_IDs;
+    static std::set<ElementID> freed_IDs;// nie wiem jak to ma byc, to co zrobiłem tu jest chyba zgodnie z instrukcją ale co dalej? gdzie nadać to id skoro jest ten konstruktor taki
+
     Package& operator=(Package&&) = default;
-    ElementID get_id() { return id_; }
+    ElementID get_id() const { return id_; }
     ~Package() = default;
 
     ElementID id_;
--- src/package.cpp
+++ src/package.cpp
@@ -2,3 +2,6 @@
 // Created by michal on 14/12/2021.
 //
 #include "package.hpp"
+
+std::set<ElementID> Package::assigned_IDs;
+std::set<ElementID> Package::freed_IDs;
