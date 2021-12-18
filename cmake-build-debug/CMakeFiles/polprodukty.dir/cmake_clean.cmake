d69a497585fe1ada7e3e2ae9c2e9cee4cf42ea05
ttarnawski <81701027+ttarnawski@users.noreply.github.com>
2021-12-18 14:44:45 +0100

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/polprodukty.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/polprodukty.dir/main.cpp.obj"
+  "CMakeFiles/polprodukty.dir/src/package.cpp.obj"
+  "CMakeFiles/polprodukty.dir/src/storage_types.cpp.obj"
+  "libpolprodukty.dll.a"
+  "polprodukty.exe"
+  "polprodukty.exe.manifest"
+  "polprodukty.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/polprodukty.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- src/package.cpp
+++ src/package.cpp
@@ -1,4 +1,4 @@
 //
 // Created by michal on 14/12/2021.
 //
-
+#include "package.hpp"
