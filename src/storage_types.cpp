9fafe6b1c63c5d8e2d32c54127eff06dbd06a8e1
ttarnawski <81701027+ttarnawski@users.noreply.github.com>
2021-12-18 18:03:07 +0100

    Add files via upload

--- src/storage_types.cpp
+++ src/storage_types.cpp
@@ -2,3 +2,12 @@
 // Created by michal on 14/12/2021.
 //
 #include "storage_types.hpp"
+
+Package PackageQueue::pop() {
+    switch (queue_type_) {
+        case FIFO:
+            queue_.pop_back();
+        case LIFO:
+            queue_.pop_front();
+    }
+}
