789d15b0e5226dc341fad0a3f23147056f9ad569
ttarnawski <81701027+ttarnawski@users.noreply.github.com>
2022-01-03 19:50:13 +0100

    cos tam niby dziala, ale czy dobrze?

--- main.cpp
+++ main.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
 #include "package.hpp"
+#include "storage_types.hpp"
 
 int main() {
     Package a;
     Package b;
     Package c;
     Package d;
-    std::cout << a.get_id() << b.get_id() << c.get_id() << d.get_id();
+    std::cout << a.get_id() << b.get_id() << c.get_id() << d.get_id() << '\n';
+
+    PackageQueue q(FIFO);
+    std::cout << q.empty() << '\n';
+    q.push();
+    q.push();
+    std::cout << q.size() << '\n';
+    q.pop();
+    std::cout << q.size();
     return 0;
+
 
 }
