85f74e95e905316ab8f8f2a495f0955306a07bc1
ttarnawski <81701027+ttarnawski@users.noreply.github.com>
2022-01-03 19:34:31 +0100

    cos takiego??

--- src/storage_types.cpp
+++ src/storage_types.cpp
@@ -6,13 +6,16 @@
 Package PackageQueue::pop() {
     switch (queue_type_) {
         case FIFO: {
-            Package& popped = queue_.back();
+            Package popped = std::move(queue_.back());
             queue_.pop_back();
             return popped;
         }
-        case LIFO:
-            Package& popped = queue_.front();
+        case LIFO: {
+            Package popped = std::move(queue_.front());
             queue_.pop_front();
             return popped;
+        }
+        default:
+            return 0;
     }
 }
