49a61ac42f48d4051ff3b186fbf548e66c0b42ed
michalsciubisz <michalsci@student.agh.edu.pl>
2022-01-04 10:10:26 +0100

    helpers_konspekt

--- src/helpers.cpp
+++ src/helpers.cpp
@@ -2,3 +2,17 @@
 // Created by michal on 04/01/2022.
 //
 
+#include "helpers.hpp"
+
+#include <cstdlib>
+#include <random>
+
+std::random_device rd;
+std::mt19937 rng(rd());
+
+double default_probability_generator() {
+    // Generuj liczby pseudolosowe z przedziału [0, 1); 10 bitów losowości.
+    return std::generate_canonical<double, 10>(rng);
+}
+
+std::function<double()> probability_generator = default_probability_generator;
