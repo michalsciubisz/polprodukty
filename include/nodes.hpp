e7376a077ba9f55e7007b765b1df047020a3ba70
tomis <ttarnawski@student.agh.edu.pl>
2022-01-09 16:35:05 +0100

    aaaa

--- include/nodes.hpp
+++ include/nodes.hpp
@@ -4,16 +4,28 @@
 
 #ifndef MAIN_CPP_NODES_HPP
 #define MAIN_CPP_NODES_HPP
+
+#include <memory>
+#include <map>
 #include "types.hpp"
 #include "package.hpp"
 #include "storage_types.hpp"
 
-class IPackageReceiver{
+class IPackageReceiver {
     virtual void receive_package(Package&& p) = 0;
     virtual ElementID get_id() const = 0;
+
+    // W klasie IPackageReceiver nie musisz definiować nowych aliasów na typ std::list<Package>::const_iterator “od zera” –
+    // możesz zdefiniować go w oparciu o alias IPackageStockpile::const_iterator
+    // (albo skorzystać wprost z IPackageStockpile::const_iterator bezpośrednio w nagłówkach metod).
+    // tylko do czego maja sie odnosic te itaratory?
+    IPackageStockpile::const_iterator begin() const { return stockpile_.cbegin(); }
+    IPackageStockpile::const_iterator cbegin() const { return stockpile_.cbegin(); }
+    IPackageStockpile::const_iterator end() const { return stockpile_.cend(); }
+    IPackageStockpile::const_iterator cend() const { return stockpile_.cend(); }
 };
 
-class ReceiverPreferences{
+class ReceiverPreferences {
 public:
     using preferences_t = std::map<IPackageReceiver*, double>;
     using const_iterator = preferences_t::const_iterator;
@@ -24,21 +36,28 @@ public:
     IPackageReceiver* choose_receiver();
     preferences_t& get_preferences() {}
     ProbabilityGenerator pg_;
+
+private:
+    preferences_t preferences_t_;
 };
 
-class PackageSender : ReceiverPreferences{
+class PackageSender: public ReceiverPreferences {
 public:
     PackageSender(PackageSender&&) = default;
 
-    ReceiverPreferences receiver_preferences_;
     void send_package();
     std::optional<Package>& get_sending_buffer();
+
 protected:
     void push_package(Package&&);
+
+private:
+    ReceiverPreferences receiver_preferences_;
 };
 
-class Ramp : PackageSender{
+class Ramp: public PackageSender {
 public:
+
     Ramp(ElementID id, TimeOffset di) : id_(id), di_(di) {}
     void deliver_goods(Time t);
     TimeOffset get_delivery_interval() {return di_;}
@@ -47,19 +66,19 @@ public:
     TimeOffset di_;
 };
 
-class Worker : IPackageReceiver{
+class Worker: public PackageSender, public IPackageReceiver, public IPackageQueue {
 public:
     Worker(ElementID id, TimeOffset pd, std::unique_ptr<IPackageQueue> q) : id_(id), pd_(pd), q_(q) {}
     void do_work(Time t);
-    TimeOffset get_processing_duration();
-    Time get_package_processing_start_time();
+    TimeOffset get_processing_duration() { return pd_; }
+    Time get_package_processing_start_time() { return id_; }
 
     ElementID id_;
     TimeOffset pd_;
     std::unique_ptr<IPackageQueue> q_;
 };
 
-class Storehouse : IPackageReceiver{
+class Storehouse: public IPackageReceiver, public IPackageStockpile {
     Storehouse(ElementID id, std::unique_ptr<IPackageStockpile> d) : id_(id), d_(d) {}
     ElementID id_;
     std::unique_ptr<IPackageStockpile> d_;
--- src/helpers.cpp
+++ src/helpers.cpp
@@ -7,12 +7,12 @@
 #include <cstdlib>
 #include <random>
 
-//std::random_device rd;
-//std::mt19937 rng(rd());
-//
-//double default_probability_generator() {
-//    // Generuj liczby pseudolosowe z przedziału [0, 1); 10 bitów losowości.
-//    return std::generate_canonical<double, 10>(rng);
-//}
-//
-//std::function<double()> probability_generator = default_probability_generator;
+std::random_device rd;
+std::mt19937 rng(rd());
+
+double default_probability_generator() {
+    // Generuj liczby pseudolosowe z przedziału [0, 1); 10 bitów losowości.
+    return std::generate_canonical<double, 10>(rng);
+}
+
+std::function<double()> probability_generator = default_probability_generator;
