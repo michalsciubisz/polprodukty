5df141dbfe9318e1074dc52240a9e9b60f2b99ff
Wojciech Tokarz <wtokarz@student.agh.edu.pl>
2022-01-17 21:43:48 +0100

    simulation zrobione

--- include/simulation.hpp
+++ include/simulation.hpp
@@ -4,5 +4,8 @@
 
 #ifndef MAIN_CPP_SIMULATION_HPP
 #define MAIN_CPP_SIMULATION_HPP
+#include "reports.hpp"
+
+void simulate(Factory& factory, TimeOffset d, std::function<void (Factory&, Time)> rf);
 
 #endif //MAIN_CPP_SIMULATION_HPP
